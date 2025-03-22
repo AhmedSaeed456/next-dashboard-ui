0ee3c8df689d874ffd2792b5f8fb51da83a873c7
Ahmed Saeed <mrsaytama000@gmail.com>
2025-03-22 17:37:43 +0200

    add assetsPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/next-dashboard-ui" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/next-dashboard-ui" : "",
 };
 
 export default nextConfig;
